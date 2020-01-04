494f03b49a37f005f8103e8f2e77842f66b61e3f
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2020-01-03 19:10:59 -0800

    py3 compat

--- pan_consts.py
+++ pan_consts.py
@@ -1,16 +1,8 @@
-# --
 # File: pan_consts.py
+# Copyright (c) 2014-2020 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2014-2016
-#
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber.
-#
-# --
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 
 PAN_ERR_REPLY_FORMAT_KEY_MISSING = "None '{key}' missing in reply from device"
 PAN_ERR_REPLY_NOT_SUCCESS = "REST call returned '{status}'"
